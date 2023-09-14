357709584a0f96fada06a666a2a395e0922dac24
Takanori TANIGUCHI <askme@pretendco.net>
2023-09-14 11:32:50 +0900

    fix and fmted

--- renew_keytab.sh
+++ renew_keytab.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
-HN=`hostname`
+HN="$(hostname)"
 echo "kinit administrator"
 kinit administrator
 echo ""
 
-KEYTAB=`klist -k | grep krb5.keytab | awk -F: '{print $3}'`
+KEYTAB="$(klist -k | grep krb5.keytab | awk -F: '{print $3}')"
 echo "Check current KVNO of ${KEYTAB}."
 echo "----------------------------------"
-CKVNO=`klist -k | grep $HN | awk '{print $1}' | head -1`
+CKVNO="$(klist -k | grep "$HN" | awk '{print $1}' | head -1)"
 echo "KVNO is ${CKVNO} in ${KEYTAB}."
-SPN=`  klist -k | grep $HN | awk '{print $2}' | head -1 | awk -F@ '{print $1}'`
-KDC_KVNO=`kvno $SPN | awk -F= '{print $2}' | tr -d " "`
+SPN="$(klist -k | grep "$HN" | awk '{print $2}' | head -1 | awk -F@ '{print $1}')"
+KDC_KVNO="$(kvno "$SPN" | awk -F= '{print $2}' | tr -d " ")"
 echo "KVNO is ${KDC_KVNO} in KDC"
 
-if [ ${CKVNO} -eq  ${KDC_KVNO} ]; then
-        echo "It's ok. Nothing to do now."
-        exit 0
+if [ "${CKVNO}" -eq "${KDC_KVNO}" ]; then
+    echo "It's ok. Nothing to do now."
+    exit 0
 else
-        mv ${KEYTAB} /tmp/KEYTAB.$$
-        chmod 400 /tmp/KEYTAB.$$
+    mv "${KEYTAB}" /tmp/KEYTAB.$$
+    chmod 400 /tmp/KEYTAB.$$
 
-        net ads keytab create
-        chmod 644 ${KEYTAB}
+    net ads keytab create
+    chmod 644 "${KEYTAB}"
 
-        CKVNO=`klist -k | grep $HN | awk '{print $1}' | head -1`
-        KDC_KVNO=`kvno $SPN | awk -F= '{print $2}' | tr -d " "`
-        echo "KVNO is ${CKVNO} in ${KEYTAB}."
-        echo "KVNO is ${KDC_KVNO} in KDC"
-        if [ ${CKVNO} -eq  ${KDC_KVNO} ]; then
-                echo "Synced OK."
-                exit 0
-        else
-                echo "Something wrong. Hummm...."
-                exit 1
-        fi
+    CKVNO="$(klist -k | grep "$HN" | awk '{print $1}' | head -1)"
+    KDC_KVNO="$(kvno "$SPN" | awk -F= '{print $2}' | tr -d " ")"
+    echo "KVNO is ${CKVNO} in ${KEYTAB}."
+    echo "KVNO is ${KDC_KVNO} in KDC"
+    if [ "${CKVNO}" -eq "${KDC_KVNO}" ]; then
+        echo "Synced OK."
+        exit 0
+    else
+        echo "Something wrong. Hummm...."
+        exit 1
+    fi
 fi
